131e7d9d7d7926960042ff6de54bf9fff6a6b1b1
anddy0622 <anddy0622@gmail.com>
2022-08-31 15:15:09 -0400

    major udpate

--- AutoUpdateNotion_API.py
+++ AutoUpdateNotion_API.py
@@ -21,11 +21,10 @@ from myPackage import NotionUpdate_API as N_Update
 from myPackage import change_background as cb
 from myPackage import Monthly_Eval as pMon
 from myPackage import Read_Data as NRD
-import Notion_API as N_API
+from Notion_API import ConnectNotionDB as Connect_NotionAPI
 
 # Modify the data for git representation(Privacy reasons)
 from myPackage import remove_names_git
-
 
 
 class Connect_Notion:
@@ -35,17 +34,17 @@ class Connect_Notion:
 
         # Get Task Schedule Data -> task_data
         self.task_databaseId = secret.task_scheduleDB("database_id")
-        TASK = N_API.ConnectNotionDB(self.task_databaseId, self.token_key)
+        TASK = Connect_NotionAPI(self.task_databaseId, self.token_key)
         self.task_data = TASK.retrieve_data()
 
         # Get Evaluation Data -> eval_data
         self.eval_databaseId = secret.evaluation_db("database_id")
-        EVAL = N_API.ConnectNotionDB(self.eval_databaseId, self.token_key)
+        EVAL = Connect_NotionAPI(self.eval_databaseId, self.token_key)
         self.eval_data = EVAL.retrieve_data()
 
         # Get Duration Data -> dur_data
         self.duration_databaseId = secret.durationDB('database_id')
-        DUR = N_API.ConnectNotionDB(self.duration_databaseId, self.token_key)
+        DUR = Connect_NotionAPI(self.duration_databaseId, self.token_key)
         self.dur_data = DUR.retrieve_data()    
 
         self.headers = {
@@ -143,12 +142,18 @@ class Connect_Notion:
         weekday = CNotion.get_today("weekday")
         
         print("****************** Updating Today's Schedule ******************")
+
+        # Set up progress bar variables
+        l = len(self.task_data['Name'])
+        #Connect_NotionAPI.printProgressBar(0, l, prefix = 'Task Update Progress: ', suffix = 'Complete')
+
         for block in range(len(self.task_data['Name'])):
             
             # If it's past 1:00 pm, don't reschedule it again
             ## Fix any changes made to the automated schedule
-            if CNotion.is_time_between(time_time(13,00),time_time(2,00)) == True:
-                return self.task_data['Status'].count("Today")
+            #if CNotion.is_time_between(time_time(13,00),time_time(2,00)) == True:
+            #    print('\n\n')
+            #    return self.task_data['Status'].value_counts()["Today"]
             
             
             # Check if today(Mon,Tue,...,Sun) matches the block's day
@@ -187,6 +192,8 @@ class Connect_Notion:
                 if self.task_data["Status"] != "Today":
                     CNotion.updateTask_to_today(self.task_data["pageId"][block])
                     print("[%s] Block Updated" % self.task_data["Name"][block])
+            
+            #Connect_NotionAPI.printProgressBar(block + 1, l, prefix = 'Task Update Progress: ', suffix = 'Complete')
         
         print("\nUpdate Completed\n\n\n\n")
         
@@ -213,17 +220,16 @@ class Connect_Notion:
         pMon.monthly_eval(projects_data_eval, update_window = True) # Replace it with projects_data_eval
         if os.name != 'posix':
             cb.update_Background() # Change the windows background with the self-evaluation IMG 
-        #N_Update.uploadEvaluationJPG()
         print('Upload Completed\n\n\n\n')
         
         # Save to D Drive (if plugged in) for further statistical analysis
         RDATA = NRD.read_data()
         all_dat = RDATA.all_data("include date")[0]
         RDATA.save_to_Ddrive(all_dat)
+        # Save another img with update_window == False in the D drive
+        pMon.monthly_eval(projects_data_eval, update_window = False) 
 
 
-        
-    
     def is_time_between(self, begin_time, end_time, check_time=None):
         # If check time is not given, default to current UTC time
         check_time = check_time or datetime.now().time()
@@ -232,44 +238,23 @@ class Connect_Notion:
         else: # crosses midnight
             return check_time >= begin_time or check_time <= end_time
     
+    def execute_all(self):
+        # Update Schedule
+        self.update_Schedule()
+
+        ##### Update Duration DB #####
+        import notion_durationDB 
+
+        # Download the evaluation data
+        self.download_evaluationCSV()
+
+        # Upload Evaluation Visualization 
+        self.update_evaluationJPG()
 
 
 # Schedule my tasks 
 CNotion = Connect_Notion()
-
-# Update Schedule
-CNotion.update_Schedule()
-
-# Update Total Duration Estimate Database
-# Reconnect to get updated database
-
-
-##### Update Duration DB #####
-import notion_durationDB     #
-##############################
-
-# Download the evaluation data
-CNotion.download_evaluationCSV()
-
-# Upload Evaluation Visualization 
-CNotion.update_evaluationJPG()
-
-
-# Update Visualization for monthly evaluation (D Drive)
-try:
-    if os.name == 'posix':
-        sys.path.append(r'/Volumes/Programming/Personal/progress')
-    else:
-        sys.path.append(r'D:\Personal\progress\notion_automation')
-    print("****************** Uploading evaluation.jpg file ******************")
-    mon = pRd.read_data()
-    cur_month = datetime.now().month
-    cur_year = int(datetime.now().strftime('%y'))
-    month = mon.monthly(cur_month, cur_year)
-    pMon.monthly_eval(month)
-    print("\nD drive evaluation JPG file Updated\n\n")
-except:
-    pass
+CNotion.execute_all()
 
 
 
@@ -278,7 +263,10 @@ except:
 
 
 
-        
+
+
+
+
         
         
         
